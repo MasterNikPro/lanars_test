b2e06ca7fd3d1b00bb7a60caa80f107c68f5a242
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-03-28 21:08:34 +0300

    Creating file unsplash_api_service.dart which makes a request for getting photo from unsplash api

---
+++ lib/sevices/unsplash_api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:lanars_test/models/unsplash_image.dart';
+class UnsplashService {
+  final _dio = Dio(BaseOptions(baseUrl: 'https://api.unsplash.com'));
+  final String _apiKey= "7Y9nimADYZuUc8ZO1CqaGTLGlc-_BGz71u6nXW9utAw";
+  Future<List<UnsplashImage>> getPhotos() async {
+    try {
+      final response = await _dio.get('/photos', queryParameters: {'client_id': _apiKey});
+      final data = response.data as List<dynamic>;
+      return data.map((unsplashImage) => UnsplashImage.fromJson(unsplashImage)).toList();
+    } on DioError catch (error) {
+      throw error.message ?? "An error occurred while making the request";
+    }
+  }
+}
