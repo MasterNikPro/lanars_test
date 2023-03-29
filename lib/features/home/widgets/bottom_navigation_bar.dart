0db2032e9d6acab5075806ccbac0cfe24bd27979
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-03-29 19:02:10 +0300

    Bottom navigation complete and navigation to the screens added

--- lib/features/home/widgets/bottom_navigation_bar.dart
+++ lib/features/home/widgets/bottom_navigation_bar.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:lanars_test/features/home/screens/home_screen.dart';
+import 'package:lanars_test/features/home/screens/search_screen.dart';
 
 import '../../../utils/bottom_navigation_bar_elements.dart';
 
 class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int _selectedIndex = 0;
+    int? _selectedIndex;
     return BottomNavigationBar(
       items: bottom_navigation_items,
-      currentIndex: _selectedIndex,
+      currentIndex: _selectedIndex == null ? 0 : _selectedIndex,
       selectedItemColor: Colors.amber[800],
       onTap: (int index) {
-        switch(index){
+        _selectedIndex = index;
+        switch (index) {
           case 0:
-            if(_selectedIndex==index){
-              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+            if (_selectedIndex == index) {
+              Navigator.of(context).pushAndRemoveUntil(
+                  _createRoute(HomePage()), (route) => false);
             }
             break;
           case 1:
-            Navigator.pushNamedAndRemoveUntil(context, '/search_screen', (route) => false);
+            if (_selectedIndex == index) {
+              Navigator.of(context).pushAndRemoveUntil(
+                  _createRoute(SearchScreen()), (route) => false);
+            }
         }
       },
     );
   }
+}
+
+Route _createRoute(Widget route) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => route,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
